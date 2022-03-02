4a5925bc86344f000f6a6de46325b9cd0a9b1efb
PedroSOliveira <pedrovinicius35@hotmail.com>
2022-03-02 08:45:20 -0300

    Adicionando Dark theme.

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -6,9 +6,9 @@ export const Container = styled(View)`
     align-items: center;
     background: ${({ theme }) => theme.colors.yellow};
     font-size: 25px;
-    height: 60px;
+    height: 80px;
     justify-content: center;
-    margin-top: ${getStatusBarHeight() + 0}px;
+    margin-top: ${getStatusBarHeight() + Platform.OS === "ios" ? 0 : -10}px;
     padding: 10px;
     width: 100%;
 `;
